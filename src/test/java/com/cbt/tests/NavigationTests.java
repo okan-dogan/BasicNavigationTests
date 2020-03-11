4b9ec9935479d23f224b94f85709cb95756e14e8
okandogan <okan2317@gmail.com>
2020-03-11 01:00:40 -0400

    test

--- src/test/java/com/cbt/tests/NavigationTests.java
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -1,4 +1,50 @@
 package com.cbt.tests;
 
+import com.cbt.utilities.BrowserFactory;
+import com.cbt.utilities.BrowserUtils;
+import com.cbt.utilities.StringUtility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class NavigationTests {
+
+    public static void main(String[] args) {
+
+        WebDriver driverChr = BrowserFactory.getDriver("chrome");
+        driverChr.get("http://google.com");
+        BrowserUtils.wait(2);
+
+        String titleG = driverChr.getTitle();
+        System.out.println("titleG = " + titleG);
+
+        driverChr.get("http://etsy.com");
+        BrowserUtils.wait(2);
+
+        String titleE = driverChr.getTitle();
+        System.out.println("titleE = " + titleE);
+
+        driverChr.navigate().back();
+        BrowserUtils.wait(1);
+        StringUtility.verifyEquals(titleE,titleG);
+        driverChr.navigate().forward();
+        BrowserUtils.wait(1);
+        StringUtility.verifyEquals(titleE,titleE);
+
+
+        BrowserUtils.wait(2);
+        driverChr.quit();
+//#################################
+
+
+//        WebDriver driverEdg = BrowserFactory.getDriver("edge");
+//        driverEdg.get("http://google.com");
+//        BrowserUtils.wait(2);
+//
+//
+//        BrowserUtils.wait(2);
+//        driverEdg.quit();
+
+
+
+    }
 }
--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -1,15 +1,32 @@
 package com.cbt.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BrowserFactory {
 
-//    public static WebDriver getDriver(String browserName){
-//
-//        if(browserName.equalsIgnoreCase("safari")){
-//            return null;
-//        }
-//
-//
-//
-//
-//    }
+    public static WebDriver getDriver(String browserName){
+
+        if(browserName.equalsIgnoreCase("safari")){
+            return null;
+        }else if(browserName.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().version("79").setup();
+            return new ChromeDriver();
+        }else if(browserName.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else if (browserName.equalsIgnoreCase("edge")){
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+        }else{
+            System.out.println("There is no such that browser! Please, check if there is any typo.");
+            return null;
+        }
+
+
+
+    }
 }
