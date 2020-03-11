5fcc4a40afcfeb7ff5f43fa9d6b693a74e0a7213
okandogan <okan2317@gmail.com>
2020-03-10 23:58:31 -0400

    first commit

---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,15 @@
+package com.cbt.utilities;
+
+public class BrowserFactory {
+
+//    public static WebDriver getDriver(String browserName){
+//
+//        if(browserName.equalsIgnoreCase("safari")){
+//            return null;
+//        }
+//
+//
+//
+//
+//    }
+}
